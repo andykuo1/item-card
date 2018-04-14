90f17bccb4a03b9959edd8278a151bd70903abe8
Andrew <andykuo1supergreen@gmail.com>
2018-04-13 22:26:23 -0700

    Fixed missing injected special

--- res/specials/mystery.js
+++ res/specials/mystery.js
@@ -1,12 +1,12 @@
 module.exports = function(src) {
-  const tag = '<h2 class="item-name';
+  const tag = '<h2 class=\"item-name';
 
   var i = src.indexOf(tag);
   if (i == -1) return src;
 
-  i +=  + tag.length + 1;
+  i += tag.length;
 
-  var j = src.indexOf('"', i);
+  var j = src.indexOf('\"', i);
   if (j == -1) return src;
 
   return src.substring(0, j) + ' mystery' + src.substring(j);
